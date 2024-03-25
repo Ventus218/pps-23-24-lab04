adf34f960ff5fcaeb0639b39b2beeea9c070e525
Alessandro Venturini <ales.ventus21@gmail.com>
2024-03-25 13:39:43 +0100

    Ex4 done
    Tests should be improved

--- src/main/scala/tasks/adts/Ex3Stack.scala
+++ src/main/scala/tasks/adts/Ex3Stack.scala
@@ -1,7 +1,9 @@
 package tasks.adts
 
 import u03.Sequences.*
+import u03.Sequences.Sequence.*
 import u03.Optionals.*
+import u03.Optionals.Optional.*
 
 /*  Exercise 3: 
  *  Implement a Stack ADT
@@ -18,13 +20,18 @@ object Ex3Stacks:
     def empty[A]: Stack[A] // factory
     extension [A](stack: Stack[A])
       def push(a: A): Stack[A]
-      def pop(a: A): Optional[(A, Stack[A])]
+      def pop(): Optional[(A, Stack[A])]
       def asSequence(): Sequence[A]
   
   object StackImpl extends StackADT:
-    type Stack[A] = Nothing
-    def empty[A]: Stack[A] = ???
+    opaque type Stack[A] = Sequence[A]
+    def empty[A]: Stack[A] = Nil()
     extension [A](stack: Stack[A])
-      def push(a: A): Stack[A] = ???
-      def pop(a: A): Optional[(A, Stack[A])] = ???
-      def asSequence(): Sequence[A] = ???
+
+      def push(a: A): Stack[A] = Cons(a, stack)
+
+      def pop(): Optional[(A, Stack[A])] = stack match
+        case Cons(h, t) => Just((h, t))
+        case _ => Empty()
+
+      def asSequence(): Sequence[A] = stack
--- src/test/scala/tasks/adts/StackTest.scala
+++ src/test/scala/tasks/adts/StackTest.scala
@@ -17,6 +17,9 @@ class Stacktest:
 
   import stack.*
 
+  @Test def testAsSequence() =
+    assertEquals(Sequence.Cons(3, Sequence.Cons(2, Sequence.Nil())), empty[Int].push(2).push(3))
+
   @Test def testEmpty() =
     assertEquals(Sequence.Nil(), empty[Int].asSequence())
 
@@ -24,7 +27,30 @@ class Stacktest:
     assertEquals(Sequence.Cons(10, Sequence.Nil()), empty[Int].push(10).asSequence())
 
   @Test def testPopOnEmpty() =
-    assertEquals(Optional.Empty(), empty[Int].pop(10))
+    assertEquals(Optional.Empty(), empty[Int].pop())
 
   @Test def testPopOnNotEmpty() =
-    assertEquals(Optional.Just((10, Sequence.Nil())), empty[Int].push(10).pop(10))
+    assertEquals(Optional.Just((10, Sequence.Nil())), empty[Int].push(10).pop())
+
+  @Test def testMultiplePops() =
+    val stack = empty[Int].push(1).push(2)
+
+    val stack2 = stack.pop() match
+      case Optional.Just((value, stack)) =>
+        assertEquals(2, value)
+        stack
+      case _ =>
+        fail("Expected 2 got empty stack")
+        stack
+
+    val _ = stack2.pop() match
+      case Optional.Just((value, stack)) =>
+        assertEquals(1, value)
+        stack
+      case _ => fail("Expected 1 got empty stack")
+
+    /*val (pop2, _) = stack.pop()
+    val (pop1, _) = stack.pop()
+    assertEquals(Optional.Just(2), pop2)
+    assertEquals(Optional.Just(1), pop1)*/
+    
