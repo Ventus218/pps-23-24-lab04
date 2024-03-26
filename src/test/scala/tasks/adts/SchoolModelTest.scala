164abfaa2e79966665687aa974282ac53187faf5
Alessandro Venturini <ales.ventus21@gmail.com>
2024-03-26 17:25:37 +0100

    added problem example

--- src/test/scala/tasks/adts/SchoolModelTest.scala
+++ src/test/scala/tasks/adts/SchoolModelTest.scala
@@ -6,11 +6,22 @@ import u03.Sequences.Sequence.*
 import SchoolModel.*
 import SchoolModel.given
 import SchoolModuleImpl._school
+import SchoolModuleImpl._teacher
+import SchoolModuleImpl._course
 
 class SchoolModelTest:
 
     val module = summon[SchoolModule]
     import module.*
 
+    val teacherName = "t"
+    val t = _teacher(teacherName, Nil())
+    val courseName = "c"
+    val c = _course(courseName)
+
     @Test def newSchool(): Unit =
         assertEquals(_school(Nil(), Nil()), school())
+    
+    @Test def testAddTeacher(): Unit =
+        val c = school().nameOfTeacher(t)
+        assertEquals(_school(Cons(t, Nil()), Nil()), school().addTeacher(teacherName))
